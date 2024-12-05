a49cff8724380404c7b72a8894f7c2fb049da1de
Oxygen1282 <dzarkovacki03@gmail.com>
2024-12-05 22:20:13 +0000

    Assignment 7

--- docker_demo/assignment.sh
+++ docker_demo/assignment.sh
@@ -1,4 +1,10 @@
-#! /bin/usr/env .sh
-#
-echo Hello World!
+#! /bin/bash
 
+apt update
+apt install -y nginx
+apt install -y vim
+service nginx start
+
+sed -i 's/Welcome to nginx/Welcome to Davids page/' /var/www/html/*.html
+
+sed -i 's/nginx/nginx (Pronounced as EngineX)/g' /var/www/html/*.html
